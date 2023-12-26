9c05c2d51ca9f2be6fe46fd4d6b5f64fff3dcd0c
Dejan Camiller <64418367+dejanMT@users.noreply.github.com>
2023-12-26 21:05:49 +0100

    Craeting classes and properties

---
+++ Domain/Models/Flight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Flight
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+        public DateTime DepartureDate { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public string CountryFrom { get; set; }
+        public string CountryTo { get; set; }
+        public decimal WholesalePrice { get; set; }
+        public double CommissionRate { get; set; }
+    }
+}
---
+++ Domain/Models/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public  class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        [ForeignKey("Flight")]
+        public int FlightIdFK { get; set; }
+        public virtual Flight Flight { get; set; }
+
+        public string Passport { get; set; }
+        public decimal PricePaid { get; set; }
+        public bool Cancelled { get; set; }
+    }
+}
